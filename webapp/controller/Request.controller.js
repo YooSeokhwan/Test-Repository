fd97b135ad06e8cdf45575dab81cab2f4d4fd9b1
Seok Hwan Yoo <ysh50227@gmail.com>
2025-04-10 23:48:47 +0000

    Filtering

--- webapp/controller/Request.controller.js
+++ webapp/controller/Request.controller.js
@@ -1,9 +1,57 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "../model/formatter"
-], function (Controller, formatter) {
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+], function (Controller, formatter, Filter, FilterOperator) {
     "use strict";
     return Controller.extend("project1.controller.Request", {
-        formatter: formatter
+        formatter: formatter,
+
+onSearch: function () {
+    let ReqNum = this.byId("ReqNum").getValue();
+    let ReqGood = this.byId("ReqGood").getValue();
+    let Requester = this.byId("Requester").getValue();
+    let ReqDateObj = this.byId("ReqDate").getDateValue(); // Date 객체로 받음
+    let ReqStatus = this.byId("ReqStatus").getSelectedKey();
+
+    let ReqDate = "";
+    if (ReqDateObj instanceof Date && !isNaN(ReqDateObj)) {
+        const yyyy = ReqDateObj.getFullYear();
+        const mm = (ReqDateObj.getMonth() + 1).toString().padStart(2, '0');
+        const dd = ReqDateObj.getDate().toString().padStart(2, '0');
+        ReqDate = `${yyyy}-${mm}-${dd}`;
+    }
+
+    const aFilter = [];
+
+    if (ReqNum) {
+        aFilter.push(new Filter("ReqNum", FilterOperator.Contains, ReqNum));
+    }
+    if (ReqGood) {
+        aFilter.push(new Filter("ReqGood", FilterOperator.Contains, ReqGood));
+    }
+    if (Requester) {
+        aFilter.push(new Filter("Requester", FilterOperator.Contains, Requester));
+    }
+    if (ReqDate) {
+        aFilter.push(new Filter("ReqDate", FilterOperator.Contains, ReqDate));
+    }
+    if (ReqStatus) {
+        aFilter.push(new Filter("ReqStatus", FilterOperator.Contains, ReqStatus));
+    }
+
+    const oTable = this.byId("RequestTable").getBinding("rows");
+    oTable.filter(aFilter);
+},
+
+        onReset : function() {
+            this.byId("ReqNum").setValue("");
+            this.byId("ReqGood").setValue("");
+            this.byId("Requester").setValue("");
+            this.byId("ReqDate").setValue("");
+            this.byId("ReqStatus").setSelectedKey("");
+            this.onSearch();
+            }            
     });
 });
