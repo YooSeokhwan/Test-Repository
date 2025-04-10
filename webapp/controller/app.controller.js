e7b24a3fbad95557b2d31fe8d15d76cea51b1246
Seok Hwan Yoo <ysh50227@gmail.com>
2025-04-10 12:18:55 +0000

    Nested Views

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -5,14 +5,5 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("project1.controller.app", {
-        onShowHello :function () {
-            // i18n 모델로부터 메시지 읽기
-            let oBundle = this.getView().getModel("i18n").getResourceBundle();
-            let sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            let sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-            //메시지 보이기
-            MessageToast.show(sMsg);
-        }
     });
 });
