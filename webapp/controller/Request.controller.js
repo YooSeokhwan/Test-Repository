f46f5f43a4d7b3aa25c892a9135a8b342c137c25
Seok Hwan Yoo <ysh50227@gmail.com>
2025-04-10 17:11:14 +0000

    Custom Formatters

--- webapp/controller/Request.controller.js
+++ webapp/controller/Request.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "../model/formatter"
+], function (Controller, formatter) {
     "use strict";
     return Controller.extend("project1.controller.Request", {
+        formatter: formatter
     });
 });
