d029c6e81f5a89d53ecc33334b12c432e8b69e99
Seok Hwan Yoo <ysh50227@gmail.com>
2025-04-10 13:25:57 +0000

    Fragment Callbacks

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,17 +1,33 @@
 sap.ui.define([
-        "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast"
-     ], function (Controller, MessageToast) {
-        "use strict";
-        return Controller.extend("project1.controller.HelloPanel", {
-           onShowHello : function () {
-              // read msg from i18n model
-              var oBundle = this.getView().getModel("i18n").getResourceBundle();
-              var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-              var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-              // show message
-              MessageToast.show(sMsg);
-           }
-        });
-     });
+        "sap/ui/core/mvc/Controller",
+        "sap/m/MessageToast",
+        "sap/ui/core/Fragment"
+     ], function(Controller, MessageToast, Fragment) {
+        "use strict";
+        return Controller.extend("project1.controller.HelloPanel", {
+            onShowHello : function () {
+            // read msg from i18n model
+            let oBundle = this.getView().getModel("i18n").getResourceBundle();
+            let sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            let sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            // show message
+            MessageToast.show(sMsg);
+            },
+            onOpenDialog: function() {
+                //create dialog lazily
+                if (!this.pDialog) {
+                    this.pDialog = this.loadFragment({
+                        name: "project1.view.fragment.HelloDialog"
+                    });
+                }
+                this.pDialog.then(function (oDialog) {
+                    oDialog.open();
+                })
+            },
+            onCloseDialog : function () {
+                this.byId("helloDialog").close();
+                this.pDialog = null;
+            }
+        });
+     });
     
